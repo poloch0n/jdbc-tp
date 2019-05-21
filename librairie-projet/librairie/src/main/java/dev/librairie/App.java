affd2fd8fed243f215b341d4002dbaa679591e55
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-21 14:45:28 +0200

    fin tp01

--- librairie-projet/librairie/src/main/java/dev/librairie/App.java
+++ librairie-projet/librairie/src/main/java/dev/librairie/App.java
@@ -1,8 +1,15 @@
 package dev.librairie;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Query;
 
 import dev.librairie.module.Livre;
 
@@ -29,7 +36,19 @@ public class App
             } else {
             	System.out.println("livre pas trouvé");
             }
-            
+
+			TypedQuery<Livre>query = em.createQuery("select l from Livre l where l.titre like :titre ",Livre.class);
+			query.setParameter("titre", "Apprendre à parler aux animaux");
+
+			try {
+				Livre l2 = query.getSingleResult();
+            	System.out.println("livre trouvé");
+            	System.out.println(l2.getAuteur());
+            	System.out.println(l2.getTitre());
+            }catch(Exception e){
+            	System.out.println("livre pas trouvé");
+            }
+
         } catch(Exception e) {
         	
         }
